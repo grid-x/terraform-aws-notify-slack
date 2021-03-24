7f12ca7a9073732cb60402903954950749e1c49d
tobjaw <tobjaw@gmail.com>
2021-03-24 19:16:16 +0100

    feat: security link to cloudwatch

--- functions/notify_slack.py
+++ functions/notify_slack.py
@@ -14,6 +14,26 @@ accounts = {
   '55400ed1f95a6acd78a51f7f6efb6d5f': 'gridbox'
 }
 
+cwPrefix = 'https://eu-central-1.console.aws.amazon.com/cloudwatch/home?region=eu-central-1#logsV2:log-groups/log-group/cloudtrail-multi-region/log-events'
+cwSuffix = '$26start$3D-43200000' # last 12 hours
+cwAlarms = {
+  'UnauthorizedAPICalls': '$3FfilterPattern$3D$257B$2520($2524.errorCode$2520$253D$2520$2522*UnauthorizedOperation$2522)$2520$257C$257C$2520($2524.errorCode$2520$253D$2520$2522AccessDenied*$2522)$2520$257D',
+  'IAMChanges': '$3FfilterPattern$3D$257B($2524.eventName$253DDeleteGroupPolicy)$257C$257C($2524.eventName$253DDeleteRolePolicy)$257C$257C($2524.eventName$253DDeleteUserPolicy)$257C$257C($2524.eventName$253DPutGroupPolicy)$257C$257C($2524.eventName$253DPutRolePolicy)$257C$257C($2524.eventName$253DPutUserPolicy)$257C$257C($2524.eventName$253DCreatePolicy)$257C$257C($2524.eventName$253DDeletePolicy)$257C$257C($2524.eventName$253DCreatePolicyVersion)$257C$257C($2524.eventName$253DDeletePolicyVersion)$257C$257C($2524.eventName$253DAttachRolePolicy)$257C$257C($2524.eventName$253DDetachRolePolicy)$257C$257C($2524.eventName$253DAttachUserPolicy)$257C$257C($2524.eventName$253DDetachUserPolicy)$257C$257C($2524.eventName$253DAttachGroupPolicy)$257C$257C($2524.eventName$253DDetachGroupPolicy)$257D',
+  'SecurityGroupChanges':  '$3FfilterPattern$3D$257B$2520($2524.eventName$2520$253D$2520AuthorizeSecurityGroupIngress)$2520$257C$257C$2520($2524.eventName$2520$253D$2520AuthorizeSecurityGroupEgress)$2520$257C$257C$2520($2524.eventName$2520$253D$2520RevokeSecurityGroupIngress)$2520$257C$257C$2520($2524.eventName$2520$253D$2520RevokeSecurityGroupEgress)$2520$257C$257C$2520($2524.eventName$2520$253D$2520CreateSecurityGroup)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteSecurityGroup)$257D',
+  'VPCChanges':  '$3FfilterPattern$3D$257B$2520($2524.eventName$2520$253D$2520CreateVpc)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteVpc)$2520$257C$257C$2520($2524.eventName$2520$253D$2520ModifyVpcAttribute)$2520$257C$257C$2520($2524.eventName$2520$253D$2520AcceptVpcPeeringConnection)$2520$257C$257C$2520($2524.eventName$2520$253D$2520CreateVpcPeeringConnection)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteVpcPeeringConnection)$2520$257C$257C$2520($2524.eventName$2520$253D$2520RejectVpcPeeringConnection)$2520$257C$257C$2520($2524.eventName$2520$253D$2520AttachClassicLinkVpc)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DetachClassicLinkVpc)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DisableVpcClassicLink)$2520$257C$257C$2520($2524.eventName$2520$253D$2520EnableVpcClassicLink)$2520$257D',
+  'RouteTableChanges': '$3FfilterPattern$3D$257B$2520($2524.eventName$2520$253D$2520CreateRoute)$2520$257C$257C$2520($2524.eventName$2520$253D$2520CreateRouteTable)$2520$257C$257C$2520($2524.eventName$2520$253D$2520ReplaceRoute)$2520$257C$257C$2520($2524.eventName$2520$253D$2520ReplaceRouteTableAssociation)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteRouteTable)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteRoute)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DisassociateRouteTable)$2520$257D',
+  'NetworkGWChanges': '$3FfilterPattern$3D$257B$2520($2524.eventName$2520$253D$2520CreateCustomerGateway)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteCustomerGateway)$2520$257C$257C$2520($2524.eventName$2520$253D$2520AttachInternetGateway)$2520$257C$257C$2520($2524.eventName$2520$253D$2520CreateInternetGateway)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteInternetGateway)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DetachInternetGateway)$2520$257D',
+  'NoMFAConsoleSignin': '$3FfilterPattern$3D$257B$2520($2524.eventName$2520$253D$2520$2522ConsoleLogin$2522)$2520$2526$2526$2520($2524.additionalEventData.MFAUsed$2521$253D$2520$2522Yes$2522)$2520$257D',
+  'ConsoleSigninFailures': '$3FfilterPattern$3D$257B$2520($2524.eventName$2520$253D$2520ConsoleLogin)$2520$2526$2526$2520($2524.errorMessage$2520$253D$2520$2522Failed$2520authentication$2522)$2520$257D',
+  'CloudTrailCfgChanges': '$3FfilterPattern$3D$257B$2520($2524.eventName$2520$253D$2520CreateTrail)$2520$257C$257C$2520($2524.eventName$2520$253D$2520UpdateTrail)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteTrail)$2520$257C$257C$2520($2524.eventName$2520$253D$2520StartLogging)$2520$257C$257C$2520($2524.eventName$2520$253D$2520StopLogging)$2520$257D',
+  'S3BucketPolicyChanges': '$3FfilterPattern$3D$257B$2520($2524.eventSource$2520$253D$2520s3.amazonaws.com)$2520$2526$2526$2520(($2524.eventName$2520$253D$2520PutBucketAcl)$2520$257C$257C$2520($2524.eventName$2520$253D$2520PutBucketPolicy)$2520$257C$257C$2520($2524.eventName$2520$253D$2520PutBucketCors)$2520$257C$257C$2520($2524.eventName$2520$253D$2520PutBucketLifecycle)$2520$257C$257C$2520($2524.eventName$2520$253D$2520PutBucketReplication)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteBucketPolicy)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteBucketCors)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteBucketLifecycle)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteBucketReplication))$2520$257D',
+  'NACLChanges':  '$3FfilterPattern$3D$257B$2520($2524.eventName$2520$253D$2520CreateNetworkAcl)$2520$257C$257C$2520($2524.eventName$2520$253D$2520CreateNetworkAclEntry)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteNetworkAcl)$2520$257C$257C$2520($2524.eventName$2520$253D$2520DeleteNetworkAclEntry)$2520$257C$257C$2520($2524.eventName$2520$253D$2520ReplaceNetworkAclEntry)$2520$257C$257C$2520($2524.eventName$2520$253D$2520ReplaceNetworkAclAssociation)$2520$257D',
+  'OrganizationsChanges': '$3FfilterPattern$3D$257B+$2528$2524.eventSource+$253D+organizations.amazonaws.com$2529+$2526$2526+$2528$2528$2524.eventName+$253D+$2522AcceptHandshake$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522AttachPolicy$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522CreateAccount$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522CreateOrganizationalUnit$2522$2529+$257C$257C+$2528$2524.eventName$253D+$2522CreatePolicy$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522DeclineHandshake$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522DeleteOrganization$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522DeleteOrganizationalUnit$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522DeletePolicy$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522DetachPolicy$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522DisablePolicyType$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522EnablePolicyType$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522InviteAccountToOrganization$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522LeaveOrganization$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522MoveAccount$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522RemoveAccountFromOrganization$2522$2529+$257C$257C+$2528$2524.eventName+$253D+$2522UpdatePolicy$2522$2529+$257C$257C+$2528$2524.eventName+$253D$2522UpdateOrganizationalUnit$2522$2529$2529+$257D',
+  'RootUsage':  '$3FfilterPattern$3D$257B+$2524.userIdentity.type+$253D+$2522Root$2522+$2526$2526+$2524.userIdentity.invokedBy+NOT+EXISTS+$2526$2526+$2524.eventType$2521$253D+$2522AwsServiceEvent$2522+$257D',
+  'DisableOrDeleteCMK': '$3FfilterPattern$3D$257B+$2528$2524.eventSource+$253D+kms.amazonaws.com$2529+$2526$2526+$2528$2528$2524.eventName+$253D+DisableKey$2529+$257C$257C+$2528$2524.eventName+$253D+ScheduleKeyDeletion$2529$2529+$257D',
+  'AWSConfigChanges': '$3FfilterPattern$3D$257B+$2528$2524.eventSource+$253D+config.amazonaws.com$2529+$2526$2526+$2528$2528$2524.eventName$253DStopConfigurationRecorder$2529$257C$257C$2528$2524.eventName$253DDeleteDeliveryChannel$2529$257C$257C$2528$2524.eventName$253DPutDeliveryChannel$2529$257C$257C$2528$2524.eventName$253DPutConfigurationRecorder$2529$2529+$257D'
+}
+
 # Decrypt encrypted URL with KMS
 def decrypt(encrypted_url):
   region = os.environ['AWS_REGION']
@@ -30,18 +50,23 @@ def cloudwatch_notification(message, region):
   account = accounts[hashlib.md5(message['account'].encode("utf-8")).hexdigest()]
   states = {'OK': 'good', 'INSUFFICIENT_DATA': 'warning', 'ALARM': 'danger'}
 
+  alarmName = message['AlarmName']
+  fields = [
+      { "title": "Alarm Name", "value": alarmName, "short": True },
+      { "title": "Account", "value": account, "short": True },
+  ]
+  alarmURL = cwAlarms.get(alarmName)
+  if alarmURL:
+    fields.append({
+        "title": "Link to Logs",
+        "value": cwPrefix + alarmURL + cwSuffix,
+        "short": False
+    })
+
   return {
     "color": states[message['NewStateValue']],
     "fallback": "Alarm {} triggered".format(message['AlarmName']),
-    "fields": [
-      { "title": "Alarm Name", "value": message['AlarmName'], "short": True },
-      { "title": "Account", "value": account, "short": True },
-      {
-        "title": "Link to Alarm",
-        "value": cloudwatch_url + region + "#alarm:alarmFilter=ANY;name=" + urllib.parse.quote(message['AlarmName']),
-        "short": False
-      }
-    ]
+    "fields": fields
   }
 
 def config_notification(message):
